fb41917a1ce561375f49e065dd4e561e7c294a6f
ShilCode <2850542@gmail.com>
2023-04-30 11:07:56 +0200

    day two of practicing new concepts

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -29,3 +29,47 @@ test('Register and Login', async({page})=>{
 
 })
 
+
+test.only('E2E flow', async({page})=>{
+    await page.goto('https://rahulshettyacademy.com/client/');
+    await page.locator('#userEmail').fill('2850542@gmail.com')
+    await page.locator('#userPassword').fill('Password121!')
+    await page.locator('#login').click()
+    const products = await page.locator("div.card-body")
+    const productName = 'zara coat 3'
+    
+     await page.waitForLoadState('networkidle');
+
+
+    const count = await products.count()
+    for(let i=0; i < count; ++i){
+        if(await products.nth(i).locator('b').textContent() === productName){
+            await products.nth(i).locator('[class="fa fa-shopping-cart"]').click()
+            break;
+            
+        }
+    }
+
+
+    await page.locator('[routerlink="/dashboard/cart"]').click()
+    await page.waitForSelector('div li')
+    const bool = await page.locator("h3:has-text('zara coat 3')").isVisible()
+    expect(bool).toBeTruthy()
+
+    await page.locator('[type="button"]').last().click()
+
+    await page.locator('[class="input txt"]').first().fill('1111')
+    await page.locator('[class="input txt"]').last().fill('mohat')
+    await page.locator('[name="coupon"]').type('coupon1111')
+    // await page.locator('[type="submit"]').click()
+    await page.locator('[placeholder="Select Country"]').type('ind',{delay:100})
+  
+    await page.locator('[class="ta-item list-group-item ng-star-inserted"]').nth(1).click()
+
+    await page.locator('[class="btnn action__submit ng-star-inserted"]').click()
+    await page.waitForSelector('h1')
+
+
+
+  
+})
